d1262f2f98060642a7222c4e97676f516c134dd5
Enlysure <Enlysure@gmail.com>
2025-08-04 21:02:43 +0800

    feat: 某些情况下cancel问题

--- src/bot/callback/users.py
+++ src/bot/callback/users.py
@@ -1,18 +1,25 @@
 from asyncio import sleep
 
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, ConversationHandler
 
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-    print(query)
-    await query.edit_message_text(text="操作已取消")
+    if query:
+        await query.edit_message_text(text="操作已取消")
+        origin_message = update.effective_message.reply_to_message
+        if origin_message:
+            await context.bot.delete_message(
+                chat_id=origin_message.chat.id,
+                message_id=origin_message.message_id
+            )
+        await query.answer("操作已取消")
     await sleep(1)
     await context.bot.delete_message(
         chat_id=update.effective_chat.id,
-        message_id=query.message.message_id
+        message_id=update.effective_message.message_id
     )
-    await query.answer("操作已取消")
     # 清除用户数据
     context.user_data.clear()
+    return ConversationHandler.END
